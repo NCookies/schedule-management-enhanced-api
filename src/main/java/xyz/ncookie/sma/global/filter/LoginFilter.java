d7c73512cad8af41c31a4d8667ba28de6c2d60e6
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-01 17:18:13 +0900

    feat/#17 - LoginFilter 예외 처리
    - @ControllerAdvice는 스프링 MVC 컨트롤러 안에서 발생한 예외만 처리한다.
    - 예외가 Filter, Interceptor 등에서 발생했다면 직접 처리해줘야 한다.

--- src/main/java/xyz/ncookie/sma/global/filter/LoginFilter.java
+++ src/main/java/xyz/ncookie/sma/global/filter/LoginFilter.java
@@ -1,15 +1,20 @@
 package xyz.ncookie.sma.global.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.util.PatternMatchUtils;
 import xyz.ncookie.sma.global.data.SessionConst;
-import xyz.ncookie.sma.global.exception.BusinessException;
 import xyz.ncookie.sma.global.exception.ErrorCode;
+import xyz.ncookie.sma.global.exception.ErrorResponse;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class LoginFilter implements Filter {
@@ -33,7 +38,9 @@ public class LoginFilter implements Filter {
             HttpSession session = httpRequest.getSession(false);
 
             if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
-                throw new BusinessException(ErrorCode.LOGIN_REQUIRED);
+                log.debug("로그인 되지 않은 상태에서 서비스 접근: {}", requestURI);
+                sendErrorResponse((HttpServletResponse) servletResponse, ErrorCode.LOGIN_REQUIRED, requestURI);
+                return;
             }
 
             log.info("로그인에 성공했습니다.");
@@ -45,6 +52,24 @@ public class LoginFilter implements Filter {
     private boolean isWhiteList(String requestURI) {
 
         return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, ErrorCode errorCode, String path) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());    // LocalDateTime 타입의 직렬화를 위해 모듈 등록해야 함
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(response.getStatus());
+
+        try {
+            response.getWriter().write(
+                    mapper.writeValueAsString(ErrorResponse.of(errorCode, path))
+            );
+        } catch (IOException e) {
+            log.error("LoginFilter 응답 생성 중 에러 발생");
+            log.error("[{}]: {}", e.getClass().getSimpleName(), e.getMessage());
+        }
     }
 
 }
